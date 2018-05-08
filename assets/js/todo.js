de77fda49750cec28e3a25b90e55c5a40554069e
sumedh1894 <karande.sumedh07@gmail.com>
2018-05-08 12:36:08 -0400

    First Commit

---
+++ assets/js/todo.js
@@ -0,0 +1,28 @@
+$("ul").on("click","li",function(){
+	$(this).toggleClass("completed");
+
+});
+
+$("ul").on("click","span",function(event){
+
+	$(this).parent().fadeOut(500,function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event){
+	if(event.which===13){
+		var data=$(this).val();
+		$(this).val("");
+		$("ul").append("<li><span><i class='far fa-trash-alt'></i></span> " + data + "</li>");
+
+	};
+
+});
+
+$("#toggle-form").click(function(){
+	$("input[type='text']").fadeToggle();
+
+})
+
